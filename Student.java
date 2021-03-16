5e1d5c5d39b1f325b61cb184b8287de2ed81b773
patrikkusuanco <79904159+patrikkusuanco@users.noreply.github.com>
2021-03-16 14:55:51 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -51,17 +51,3 @@ class Student {
        gradePoint = points / (double) numberOfCredits;
    }
 }
-
-public class ShowStudent2 {
-   public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter Id");
-       String Id = sc.next();
-       System.out.println("Enter number of credits: ");
-       int cs = sc.nextInt();
-       System.out.println("Enter number of points: ");
-       int p = sc.nextInt();
-       Student s = new Student(Id, cs, p);
-       System.out.println(s);
-   }
-}
