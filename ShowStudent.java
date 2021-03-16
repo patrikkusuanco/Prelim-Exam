c04e500d27974850d9450d6e41c3b16165a0704c
patrikkusuanco <79904159+patrikkusuanco@users.noreply.github.com>
2021-03-16 14:56:56 +0800

    Create ShowStudent.java

---
+++ ShowStudent.java
@@ -0,0 +1,14 @@
+public class ShowStudent {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter Id");
+       String Id = sc.next();
+       System.out.println("Enter number of credits: ");
+       int cs = sc.nextInt();
+       System.out.println("Enter number of points: ");
+       int p = sc.nextInt();
+       Student s = new Student(Id, cs, p);
+       System.out.println(s);
+   }
+}
+
